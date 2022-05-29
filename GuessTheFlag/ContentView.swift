f0b3bcbf3f7a936aef45644692c80d47649d393e
Jeff Norton <jeff12ntn@gmail.com>
2022-05-29 10:05:55 -0600

    Initial commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jeff Norton on 5/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        
+//        LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: .white, location: 0.45), Gradient.Stop(color: .black, location: 0.55)]), startPoint: .top, endPoint: .bottom)
+        
+//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        
+//        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        
+        
+        
+//        VStack {
+//            Button {
+//                print("Button 1 was tapped")
+//            } label: {
+//                Text("Tap me!")
+//                    .padding()
+//                    .foregroundColor(.black)
+//                    .background(.yellow)
+//            }
+//                .buttonStyle(.bordered)
+//            Button {
+//                print("Edit button was tapped")
+//            } label: {
+//                Label("Edit", systemImage: "pencil")
+//            }
+//            Button("Button 2", role: .destructive) {}
+//                .buttonStyle(.bordered)
+//            Button("Button 3") {}
+//                .buttonStyle(.borderedProminent)
+//                .tint(.mint)
+//            Button("Button 4", role: .destructive) {}
+//                .buttonStyle(.borderedProminent)
+//            Button("Button 5") {}
+//                .buttonStyle(.borderless)
+//            Button("Button 6", role: .destructive) {}
+//                .buttonStyle(.borderless)
+//        }
+        
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text("Please read this.")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
